7ed8098bd792397afe30eb148c895504e580bc36
festusrono <festusrono56@gmail.com>
2025-03-25 07:51:15 +0100

    grid currencies seection

--- src/main/kotlin/CurrenciesSection.kt
+++ src/main/kotlin/CurrenciesSection.kt
@@ -1,4 +1,37 @@
 package org.example
 
-class CurrenciesSection {
-}
+import org.example.data.Currency
+
+val currencies = listOf(
+    Currency(
+        name = "USD",
+        buy = 34.32f,
+        sell = 32.32f,
+        icon = Icons.Rounded.AttachMoney
+    ),
+
+    Currency(
+        name = "EUR",
+        buy = 33.32f,
+        sell = 44.32f,
+        icon = Icons.Rounded.Euro
+    ),
+    Currency(
+        name = "USD",
+        buy = 44.34f,
+        sell = 44.88f,
+        icon = Icons.Rounded.AttachMoney
+    ),
+    Currency(
+        name = "EUR",
+        buy = 33.32f,
+        sell = 44.32f,
+        icon = Icons.Rounded.Euro
+    ),
+    Currency(
+        name = "YEN",
+        buy = 43.92f,
+        sell = 45.32f,
+        icon = Icons.Rounded.CurrencyYen
+    )
+)
